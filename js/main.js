27ef9953c94c81eb9805e3baad09e681df5415a5
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-04-21 08:20:56 -0700

    Added more validation

--- js/main.js
+++ js/main.js
@@ -3,8 +3,21 @@ window.onload = function () {
     formBtn.onclick = main;
 };
 function main() {
+    resetErrorMessages();
     validateTxtBox("first-name", "First name is required");
     validateTxtBox("last-name", "Last name is required");
+}
+function resetErrorMessages() {
+    var allSpans = document.querySelectorAll("form span");
+    for (var i = 0; i < allSpans.length; i++) {
+        var currSpan = allSpans[i];
+        if (currSpan.hasAttribute("data-required")) {
+            currSpan.innerText = "*";
+        }
+        else {
+            currSpan.innerText = "";
+        }
+    }
 }
 function validateTxtBox(id, errMsg) {
     var txtBox = document.getElementById(id);
--- ts/main.ts
+++ ts/main.ts
@@ -6,12 +6,32 @@ window.onload = function(){
 }
 
 function main():void{
+    // Resets any error messages present
+    resetErrorMessages();
+
     // First Name Validation
     validateTxtBox("first-name", "First name is required");
 
     // Last Name Validation
     validateTxtBox("last-name", "Last name is required");
 }
+
+/**
+ * Resets all spans back to the default text
+ */
+function resetErrorMessages():void{
+    let allSpans = document.querySelectorAll("form span");
+    for (let i = 0; i < allSpans.length; i++){
+        let currSpan = <HTMLElement>allSpans[i];
+        if(currSpan.hasAttribute("data-required")){
+            currSpan.innerText = "*";
+        }
+        else {
+            currSpan.innerText = "";
+        }
+    }
+}
+
 /**
  * Returns true if the textbox with the given id
  * has some text inside it 
