0f8102a5502bc5f2b8c7d414444defb0b3c9d78f
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-04-20 18:01:25 -0700

    Adding validation functions to the code

--- js/main.js
+++ js/main.js
@@ -3,5 +3,22 @@ window.onload = function () {
     formBtn.onclick = main;
 };
 function main() {
-    alert("A button was pushed, mama mia!");
+    validateFirstNameTxt();
+    validateLastNameTxt();
+}
+function validateLastNameTxt() {
+    var lastNameBox = document.getElementById("last-name");
+    var lName = lastNameBox.value;
+    if (lName == "") {
+        var errSpan = lastNameBox.nextElementSibling;
+        errSpan.innerText = "Last name is required";
+    }
+}
+function validateFirstNameTxt() {
+    var firstNameBox = document.getElementById("first-name");
+    var fName = firstNameBox.value;
+    if (fName == "") {
+        var errSpan = firstNameBox.nextElementSibling;
+        errSpan.innerText = "First name is required";
+    }
 }
--- ts/main.ts
+++ ts/main.ts
@@ -6,5 +6,27 @@ window.onload = function(){
 }
 
 function main():void{
-    alert("A button was pushed, mama mia!");
+    // First Name Validation
+    validateFirstNameTxt();
+
+    // Last Name Validation
+    validateLastNameTxt();
+}
+
+function validateLastNameTxt() {
+    let lastNameBox = <HTMLInputElement>document.getElementById("last-name");
+    let lName = lastNameBox.value;
+    if (lName == "") {
+        let errSpan = <HTMLSpanElement>lastNameBox.nextElementSibling;
+        errSpan.innerText = "Last name is required";
+    }
+}
+
+function validateFirstNameTxt() {
+    let firstNameBox = <HTMLInputElement>document.getElementById("first-name");
+    let fName = firstNameBox.value;
+    if (fName == "") {
+        let errSpan = <HTMLSpanElement>firstNameBox.nextElementSibling;
+        errSpan.innerText = "First name is required";
+    }
 }
